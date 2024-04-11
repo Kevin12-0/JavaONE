68994d67bf2e061430bd95a601519afb1b8795b0
Kevin12-0 <1719110155@utectulancingo.edu.mx>
2024-04-10 18:24:08 -0600

    varibales y textos

---
+++ catalogo.java
@@ -0,0 +1,25 @@
+public class catalogo {
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Screen Match");
+        System.out.println("Pelicula Matrix");
+        /*
+         * asignar tipo de valor en Java
+         * 
+         * tiipo str,int,float,double,bolean nameVarieble = value;
+         */
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnPlan = true;
+        double notaDeLaPelicula = 8.2;
+
+        double media = (8.2 + 6.0 + 9.0) / 3;
+        System.out.println(media);
+
+        /* cadenas de texo */
+        String sinopsis = """
+                MAtirx es una paradoja, de una de las mejores
+                peliculas de la ciecia ficcion lanzada en:
+                """ + fechaDeLanzamiento;
+        System.out.println(sinopsis);
+
+    }
+}
